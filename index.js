a5103bf15d6a1d4da29779f06e7920ad6c204359
Joseph Crump <josephcrump79@yahoo.com>
2024-10-22 08:27:49 -0500

    phase-1-algorithms-has-target-sum

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let result = false; //stores the result of whether there is a pair of numbers adding up to target
+  const n = array.length
+  for (let i = 0; i <= n - 1; i++) {
+    for (let j = 0; j <= n - 1; j++) {
+      // console.log(`${i} ${j}`)
+      if (i !== j && (array[i] + array[j] === target)) {
+        result = true
+      }
+    }
+  }
+  return result
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+  Psuedocode 
+  
+  declare a boolean value 'result' as false
+  declare n <- length of array
+  create an outer loop to go through the array index from 0 to n-1 in i 
+      create an inner loorp to go through the array index from 0 to n-1 in j
+        if i is not equal to j and element at i at j adds up to target, then
+            assign true to result
+  return the value of the result 
+ 
+ 
+ 
+  
+
 */
 
 /*
